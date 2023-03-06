c3a621e2f241cd2a3c84f7b2bffa306675346fe7
osawa-koki <github@osawa.dev>
2023-03-06 21:38:07 +0900

    小さな修正。

--- util/fn.getPrivateKey.ts
+++ util/fn.getPrivateKey.ts
@@ -1,13 +1,11 @@
 import lcm from './fn.lcm';
 
-/**
-
-公開鍵と素数二つから秘密鍵を計算する。
-@param p - 素数1。
-@param q - 素数2。
-@param publicKey - 公開鍵[n, e]。
-@returns 秘密鍵[n, d]。
-*/
+/** 公開鍵と素数二つから秘密鍵を計算する。
+ * @param p - 素数1。
+ * @param q - 素数2。
+ * @param publicKey - 公開鍵[n, e]。
+ * @returns 秘密鍵[n, d]。
+ */
 function getPrivateKey(
   p: number,
   q: number,
@@ -22,14 +20,13 @@ function getPrivateKey(
   }
   return [n, d];
 }
-/**
 
-aのmod mにおける逆元を返す。存在しない場合はエラーを返す。
-@param a - 逆元を求める数。
-@param m - 剰余の値。
-@returns aのmod mにおける逆元。
-@throws 逆元が存在しない場合、エラーを返す。
-*/
+/** aのmod mにおける逆元を返す。存在しない場合はエラーを返す。
+ * @param a - 逆元を求める数。
+ * @param m - 剰余の値。
+ * @returns aのmod mにおける逆元。
+ * @throws 逆元が存在しない場合、エラーを返す。
+ */
 function modInv(a: number, m: number): number {
   const [gcdVal, x] = extEuclidean(a, m);
   if (gcdVal !== 1) {
@@ -37,13 +34,12 @@ function modInv(a: number, m: number): number {
   }
   return ((x % m) + m) % m;
 }
-/**
 
-拡張ユークリッド互除法。ax + by = gcd(a, b)を満たす[gcd(a, b), x, y]を返す。
-@param a - 整数1。
-@param b - 整数2。
-@returns [gcd(a, b), x, y]。ax + by = gcd(a, b)を満たす。
-*/
+/** 拡張ユークリッド互除法。ax + by = gcd(a, b)を満たす[gcd(a, b), x, y]を返す。
+ * @param a - 整数1。
+ * @param b - 整数2。
+ * @returns [gcd(a, b), x, y]。ax + by = gcd(a, b)を満たす。
+ */
 function extEuclidean(a: number, b: number): [number, number, number] {
   let x = 0;
   let y = 1;
