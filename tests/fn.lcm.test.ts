ba2dc40e29d3b1d89627bf59eb75eb5db36fc4ef
osawa-koki <github@osawa.dev>
2023-03-06 20:09:26 +0900

    lcmテストコード追加した。

--- tests/fn.lcm.test.ts
+++ tests/fn.lcm.test.ts
@@ -2,11 +2,16 @@ import lcm from '../util/fn.lcm';
 
 describe('lcm', () => {
   const testCases = [
-    { inputs: [0, 0], expectedOutput: 0 },
     { inputs: [2, 3], expectedOutput: 6 },
     { inputs: [10, 25], expectedOutput: 50 },
     { inputs: [14, 21], expectedOutput: 42 },
-    { inputs: [25, 100], expectedOutput: 100 },
+    { inputs: [15, 18], expectedOutput: 90 },
+    { inputs: [35, 49], expectedOutput: 245 },
+    { inputs: [100, 125], expectedOutput: 500 },
+    { inputs: [72, 96], expectedOutput: 288 },
+    { inputs: [168, 216], expectedOutput: 1512 },
+    { inputs: [111, 123], expectedOutput: 4551 },
+    { inputs: [222, 123], expectedOutput: 9102 },
   ];
 
   test.each(testCases)(
@@ -14,6 +19,7 @@ describe('lcm', () => {
     ({ inputs, expectedOutput }) => {
       const [a, b] = inputs;
       expect(lcm(a, b)).toBe(expectedOutput);
+      expect(lcm(b, a)).toBe(expectedOutput);
     }
   );
 });
