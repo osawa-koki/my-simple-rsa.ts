4bc0b772ec5a4da1fd1221ca317d382c92c69b67
osawa-koki <github@osawa.dev>
2023-03-06 22:07:11 +0900

    encrypt関数の修正。

--- util/fn.encrypt.ts
+++ util/fn.encrypt.ts
@@ -1,13 +1,13 @@
 import modExp from './fn.modExp';
 
 /**
- * RSA暗号に基づき、指定された秘密鍵でデータを暗号化します。
- * @param privateKey - 秘密鍵[n, d]。
+ * RSA暗号に基づき、指定された公開鍵でデータを暗号化します。
+ * @param publicKey - 公開鍵[n, e]。
  * @param message - 暗号化するデータ。
  * @returns 暗号化されたデータ。
  */
-function encrypt(privateKey: [number, number], message: string): string {
-  const [n, d] = privateKey;
+function encrypt(publicKey: [number, number], message: string): string {
+  const [n, e] = publicKey;
   const blockSize = Math.floor(Math.log10(n) / Math.log10(2)) - 1;
   const blocks = message.split('').reduce((acc: string[], char: string) => {
     const code = char.charCodeAt(0);
@@ -17,7 +17,9 @@ function encrypt(privateKey: [number, number], message: string): string {
   }, []);
   const encryptedBlocks = blocks.map((block) => {
     const num = parseInt(block, 10);
-    return modExp(num, d, n).toString();
+    return modExp(num, e, n)
+      .toString()
+      .padStart(blockSize + 1, '0');
   });
   return encryptedBlocks.join('');
 }
