390eabe5f49de8f674ba6db4fa21818b6364372f
osawa-koki <github@osawa.dev>
2023-03-06 23:33:38 +0900

    不要なコード消した。

--- tests/fn.stringToAscii.test.ts
+++
@@ -1,9 +0,0 @@
-import stringToAscii from '../util/fn.stringToAscii';
-
-describe('stringToASCII', () => {
-  it('should convert a BASE64-encoded string to ASCII', () => {
-    const input = 'SGVsbG8sIHdvcmxkIQ==';
-    const expected = 'Hello, world!';
-    expect(stringToAscii(input)).toEqual(expected);
-  });
-});
