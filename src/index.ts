8309866eed27d26d10cc308b16e988617e2dfd88
osawa-koki <github@osawa.dev>
2023-03-06 22:43:28 +0900

    コマンドライン引数の解析。

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,30 @@
+import yargs from 'yargs';
 import getPublicKey from '../util/fn.getPublicKey';
 import getPrivateKey from '../util/fn.getPrivateKey';
 import encrypt from '../util/fn.encrypt';
 import decrypt from '../util/fn.decrypt';
 
-const prime1 = 61;
-const prime2 = 53;
-const message = 'Hello, world!';
+const argv = yargs(process.argv.slice(2))
+  .options({
+    prime1: { type: 'number', demandOption: true },
+    prime2: { type: 'number', demandOption: true },
+    message: { type: 'string', demandOption: true },
+  })
+  .help().argv;
 
-// 公開鍵・秘密鍵を生成する
-const publicKey = getPublicKey(prime1, prime2);
-console.log(`公開鍵: ${publicKey}`);
-const privateKey = getPrivateKey(prime1, prime2, publicKey);
-console.log(`秘密鍵: ${privateKey}`);
+Promise.resolve(argv).then(({ prime1, prime2, message }) => {
+  // 公開鍵・秘密鍵を生成する
+  const publicKey = getPublicKey(prime1, prime2);
+  console.log(`公開鍵: ${publicKey}`);
+  const privateKey = getPrivateKey(prime1, prime2, publicKey);
+  console.log(`秘密鍵: ${privateKey}`);
 
-const encrypted = encrypt(publicKey, message);
+  const encrypted = encrypt(publicKey, message);
 
-// 暗号化されたデータを復号する
-const decrypted = decrypt(privateKey, encrypted);
+  // 暗号化されたデータを復号する
+  const decrypted = decrypt(privateKey, encrypted);
 
-console.log(`元のメッセージ: ${message}`);
-console.log(`暗号化されたメッセージ: ${encrypted}`);
-console.log(`復号化されたメッセージ: ${decrypted}`);
+  console.log(`元のメッセージ: ${message}`);
+  console.log(`暗号化されたメッセージ: ${encrypted}`);
+  console.log(`復号化されたメッセージ: ${decrypted}`);
+});
