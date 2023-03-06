a217f161fe5ac69949acdfb508f6aeb79eacab3c
osawa-koki <github@osawa.dev>
2023-03-06 20:48:43 +0900

    公開鍵と秘密鍵を算出した。

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log(1);
+import isPrime from '../util/fn.isPrime';
+import getPublicKey from '../util/fn.getPublicKey';
+import getPrivateKey from '../util/fn.getPrivateKey';
+
+const args = process.argv.slice(2);
+if (args.length < 2) {
+  console.log('Usage: ts-node index.ts [prime1] [prime2]');
+  process.exit(1);
+}
+
+const p = Number(args[0]);
+const q = Number(args[1]);
+if (!isPrime(p) || !isPrime(q)) {
+  console.log('Both arguments must be prime numbers');
+  process.exit(1);
+}
+
+const publicKey = getPublicKey(p, q);
+const privateKey = getPrivateKey(p, q, publicKey);
+
+console.log(`Public key: [${publicKey[0]}, ${publicKey[1]}]`);
+console.log(`Private key: [${privateKey[0]}, ${privateKey[1]}]`);
