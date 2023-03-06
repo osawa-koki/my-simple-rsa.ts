8966de5f712397062ffbe5177266bf75667807d9
osawa-koki <github@osawa.dev>
2023-03-06 21:13:14 +0900

    小さな修正。

--- util/fn.decrypt.ts
+++ util/fn.decrypt.ts
@@ -2,14 +2,18 @@ import modExp from './fn.modExp';
 
 /**
  * 秘密鍵を用いて暗号化されたデータを復号します。
- * @param encrypted - 暗号化されたデータ。
  * @param privateKey - 復号に使用する秘密鍵。
+ * @param encrypted - 暗号化されたデータ。
  * @returns 復号されたデータ。
  */
-function decrypt(encrypted: number[], privateKey: [number, number]): string {
+function decrypt(privateKey: [number, number], encrypted: string): string {
   const [n, d] = privateKey;
-  const decryptedChars = encrypted.map((c) => modExp(c, d, n));
-  return decryptedChars.map((c) => String.fromCharCode(c)).join('');
+  const encryptedBlocks = encrypted.match(/.{1,3}/g) || [];
+  const encryptedChars = encryptedBlocks.map((block) => parseInt(block, 10));
+  const decryptedChars = encryptedChars.map((c) => modExp(c, d, n));
+  const decryptedString = String.fromCharCode(...decryptedChars);
+  return decryptedString;
 }
+
 
 export default decrypt;
