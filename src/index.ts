7791e62a31ae80b5f86aefbe4e3b79b938cb6140
osawa-koki <github@osawa.dev>
2023-03-06 21:39:52 +0900

    小さな修正。

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import isPrime from '../util/fn.isPrime';
 import getPublicKey from '../util/fn.getPublicKey';
 import getPrivateKey from '../util/fn.getPrivateKey';
 import encrypt from '../util/fn.encrypt';
@@ -10,7 +9,9 @@ const message = 'Hello, world!';
 
 // 公開鍵・秘密鍵を生成する
 const publicKey = getPublicKey(prime1, prime2);
+console.log(`公開鍵: ${publicKey}`);
 const privateKey = getPrivateKey(prime1, prime2, publicKey);
+console.log(`秘密鍵: ${privateKey}`);
 
 const encrypted = encrypt(privateKey, message);
 
