5bcc3f24da392240c37f7229c9bd858b26e0bb6d
osawa-koki <github@osawa.dev>
2023-03-06 20:15:35 +0900

    gcdのテストケースを増やした。

--- tests/fn.gcd.test.ts
+++ tests/fn.gcd.test.ts
@@ -2,11 +2,16 @@ import gcd from '../util/fn.gcd';
 
 describe('gcd', () => {
   const testCases = [
-    { inputs: [0, 0], expectedOutput: 0 },
     { inputs: [2, 3], expectedOutput: 1 },
     { inputs: [10, 25], expectedOutput: 5 },
     { inputs: [14, 21], expectedOutput: 7 },
-    { inputs: [25, 100], expectedOutput: 25 },
+    { inputs: [15, 18], expectedOutput: 3 },
+    { inputs: [35, 49], expectedOutput: 7 },
+    { inputs: [100, 125], expectedOutput: 25 },
+    { inputs: [72, 96], expectedOutput: 24 },
+    { inputs: [168, 216], expectedOutput: 24 },
+    { inputs: [111, 123], expectedOutput: 3 },
+    { inputs: [222, 123], expectedOutput: 3 },
   ];
 
   test.each(testCases)(
