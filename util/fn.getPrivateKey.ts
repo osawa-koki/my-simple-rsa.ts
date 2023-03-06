cb87e9feac7f31c0ec598fcd8f47d9c42015a59b
osawa-koki <github@osawa.dev>
2023-03-06 20:02:57 +0900

    小さな修正。

--- util/fn.getPrivateKey.ts
+++ util/fn.getPrivateKey.ts
@@ -1,12 +1,13 @@
 import lcm from './fn.lcm';
 
 /**
- * Computes the private key given two prime numbers and the public key.
- * @param p - The first prime number.
- * @param q - The second prime number.
- * @param publicKey - The public key [n, e].
- * @returns The private key [n, d].
- */
+
+公開鍵と素数二つから秘密鍵を計算する。
+@param p - 素数1。
+@param q - 素数2。
+@param publicKey - 公開鍵[n, e]。
+@returns 秘密鍵[n, d]。
+*/
 function getPrivateKey(
   p: number,
   q: number,
@@ -15,40 +16,39 @@ function getPrivateKey(
   const [n, e] = publicKey;
   const phi = lcm(p - 1, q - 1);
   let d = modInv(e, phi);
-  // Ensure d is positive
+  // dが正の数になるようにする
   while (d < 0) {
     d += phi;
   }
   return [n, d];
 }
-
 /**
- * Returns the modular multiplicative inverse of a modulo m, if it exists.
- * @param a - The number to find the inverse of.
- * @param m - The modulus.
- * @returns The modular multiplicative inverse of a modulo m, if it exists.
- * @throws An error if the inverse does not exist.
- */
+
+aのmod mにおける逆元を返す。存在しない場合はエラーを返す。
+@param a - 逆元を求める数。
+@param m - 剰余の値。
+@returns aのmod mにおける逆元。
+@throws 逆元が存在しない場合、エラーを返す。
+*/
 function modInv(a: number, m: number): number {
   const [gcdVal, x] = extEuclidean(a, m);
   if (gcdVal !== 1) {
-    throw new Error(`The modular inverse of ${a} (mod ${m}) does not exist.`);
+    throw new Error(`a = ${a}(mod ${m})における逆元が存在しません。`);
   }
   return ((x % m) + m) % m;
 }
-
 /**
- * Extended Euclidean algorithm. Returns [gcd(a, b), x, y] such that ax + by = gcd(a, b).
- * @param a - The first integer.
- * @param b - The second integer.
- * @returns An array [gcd(a, b), x, y] such that ax + by = gcd(a, b).
- */
+
+拡張ユークリッド互除法。ax + by = gcd(a, b)を満たす[gcd(a, b), x, y]を返す。
+@param a - 整数1。
+@param b - 整数2。
+@returns [gcd(a, b), x, y]。ax + by = gcd(a, b)を満たす。
+*/
 function extEuclidean(a: number, b: number): [number, number, number] {
   let x = 0;
   let y = 1;
   let u = 1;
   let v = 0;
-
   while (a !== 0) {
     const q = Math.floor(b / a);
     const r = b % a;
