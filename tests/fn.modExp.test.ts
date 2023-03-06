2b922fdb59d65b4a23a11a6a4b70b01b6a1de9e9
osawa-koki <github@osawa.dev>
2023-03-06 21:06:10 +0900

    modExpのテストケース増やした。

--- tests/fn.modExp.test.ts
+++ tests/fn.modExp.test.ts
@@ -4,6 +4,14 @@ describe('modExp', () => {
   const testCases = [
     { inputs: [2, 5, 13], expectedOutput: 6 },
     { inputs: [3, 7, 13], expectedOutput: 3 },
+    { inputs: [5, 11, 13], expectedOutput: 8 },
+    { inputs: [7, 13, 13], expectedOutput: 7 },
+    { inputs: [11, 17, 13], expectedOutput: 7 },
+    { inputs: [13, 19, 13], expectedOutput: 0 },
+    { inputs: [17, 23, 13], expectedOutput: 10 },
+    { inputs: [19, 29, 13], expectedOutput: 2 },
+    { inputs: [23, 31, 13], expectedOutput: 10 },
+    { inputs: [29, 37, 13], expectedOutput: 3 },
   ];
 
   testCases.forEach(({ inputs, expectedOutput }) => {
